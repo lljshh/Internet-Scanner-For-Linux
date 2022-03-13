e40a4757285d654e5aa418ed52c6cb8801ee05a5
lljshh <72928567+lljshh@users.noreply.github.com>
2022-03-13 11:02:47 +0800

    Delete Fun.cpp

--- Fun.cpp
+++
@@ -1,8 +0,0 @@
-/*#include "Fun.h"
-QString GetMyIP()
-{
-    QString localHostName = QHostInfo::localHostName();
-    QHostInfo info = QHostInfo::fromName(localHostName);
-    //qDebug() << "IP Address: "<< info.addresses();
-    return "test";
-}*/
